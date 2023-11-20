7059504bef9d42c3bd2dc371d770e48b84861c42
Muhammad Hassan Khan <138271415+hassanaleem125@users.noreply.github.com>
2023-11-20 15:43:49 +0500

    JS Assi5/Q3

---
+++ app.js
@@ -0,0 +1,5 @@
+var ticket = 600
+
+var buy = prompt("How many tickets do you need?")
+var total = ticket * buy
+document.write("Total cost to buy" +" " + buy  +" "+ "ticket to a movie is" +" "+total+"PKP" )
